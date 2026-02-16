c9cbcdee569c9df54cca14fb3ca84b4bedd6ba05
parthagrawal234 <personal.parth.agr.234@outlook.com>
2026-02-16 21:38:27 +0530

    Final Assignment

--- frontend/src/components/TicketCard.jsx
+++ frontend/src/components/TicketCard.jsx
@@ -68,11 +68,11 @@ const TicketCard = ({ ticket }) => {
 
             <div className="mt-2">
                 <span className={`
-    text-xs font-medium px-3 py-1 rounded-full
-    ${ticket.priority === "High" ? "bg-red-100 text-red-600" :
+                    text-xs font-medium px-3 py-1 rounded-full
+                    ${ticket.priority === "High" ? "bg-red-100 text-red-600" :
                         ticket.priority === "Medium" ? "bg-yellow-100 text-yellow-700" :
                             "bg-green-100 text-green-600"}
-  `}>
+                `}>
                     {ticket.priority} Priority
                 </span>
             </div>
